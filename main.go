850126d7d0168cc86479200e973a2a24500bcb59
rahul.r <rahul.r@i-exceed.com>
2022-12-19 20:37:51 +0530

    create account implementation

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/rahul-024/fund-transfer-poc/api"
 	"github.com/rahul-024/fund-transfer-poc/config"
 	"github.com/rahul-024/fund-transfer-poc/util"
